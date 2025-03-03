faa9e5f935a1f148e637f1a3f885358459dd0631
ercanon <61042236+ercanon@users.noreply.github.com>
2025-03-03 22:01:20 +0100

    structure style

--- docs/scripts/instanceObj.js
+++ docs/scripts/instanceObj.js
@@ -1,32 +1,45 @@
 class InstanceHandler {
+    static #parserDOM = document.createRange()
     static #struc = [
         `<button class="addBtn"></button>`,
         `<section>
-            <input type="text">
-            <input type="text">
-            <div></div>
+            <input type="text" class="section-title" placeholder = "Title">
+            <input type="text" class="section-usage" placeholder = "Usage">
          </section>`,
         `<span>
-            <input type="text">
-            <div></div>
+            <input type="text" class="span-type" placeholder = "Entry Type">
          </span>`,
-        `<iconify-icon></iconify-icon>
-         <input type="text">
-         <input type="text">`
+        `<div>
+            <iconify-icon></iconify-icon>
+            <input type="text" class="div-title" placeholder = "Title">
+            <input type="text" class="div-sub" placeholder = "Subtitle">
+         </div>`
     ];
-    #lvlStruc = null;
-    constructor(parent, lvlPrev) {
+    #sectionStyle = null;
+    constructor(parent, lvlPrev = -1) {
         if (++lvlPrev >= InstanceHandler.#struc.length)
-            return console.warn("Structure Array has reached its limit");;
+            return console.warn("Structure Array has reached its limit");
 
-        this.#lvlStruc = lvlPrev || 0;
-        parent.insertAdjacentHTML(parent.tagName === "button" ? "beforebegin" : "afterbegin", InstanceHandler.#struc[this.#lvlStruc]);
+        const newDOM = InstanceHandler.#parserDOM.createContextualFragment(InstanceHandler.#struc[lvlPrev]);
+        const lvlNextDiv = newDOM.querySelector("section, span, div");
+        if (lvlNextDiv.localName === "section") {
+            this.#sectionStyle = lvlNextDiv.style
+            this.changeColor("black");
+        }
 
-        const lvlNextDiv = parent.querySelector("div");
-        lvlNextDiv?.insertAdjacentHTML("afterbegin", InstanceHandler.#struc[0]);
+        if (lvlPrev < InstanceHandler.#struc.length -1) {
+            lvlNextDiv?.insertAdjacentHTML("beforeend", InstanceHandler.#struc[0]);
+            newDOM.querySelector("button")?.addEventListener("click", this.createStructure = () =>
+                new InstanceHandler(lvlNextDiv || parent, lvlPrev));
+        }
 
-        parent.querySelector("button").addEventListener("click", () => {
-            new InstanceHandler(lvlNextDiv || parent, this.#lvlStruc)
-        });
+        parent.insertBefore(newDOM, parent.lastChild);
+    }
+    changeColor(color) {
+        this.#sectionStyle?.setProperty("--sectionColor", color);
+    }
+    delete (){
+         parent.querySelectorAll("button")?.forEach((btn) => 
+            btn.removeEventListener("click", this.createStructure));
     }
 }
